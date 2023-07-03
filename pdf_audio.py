41f0f847228a6f309f3c36b595a26902d4413007
Ankit Patel <ankitvpatel00@gmail.com>
2023-07-03 09:49:49 -0500

    updated python script for pdf to audio

--- pdf_audio.py
+++ pdf_audio.py
@@ -1,13 +1,49 @@
-import pyttsx3, PyPDF2
+import os
+from PyPDF2 import PdfReader
+from gtts import gTTS
+import time
 
-#read pdf file
-pdfreader = PyPDF2.PdfReader(open('Principles of Mangerial Economics - Chapter 3.pdf','rb'))
-reader = pyttsx3.init()
-for page in range(len(reader.pages)):
-    text = pdfreader.getPage(page).extractText()
-    legible_text = text.strip().replace('\n',' ')
-    print(legible_text)
-    reader.say(legible_text)
-    reader.save_to_file(legible_text,'file.mp3')
-    reader.runAndWait()
-reader.stop()
+def pdf_to_text(pdf_path):
+    reader = PdfReader(pdf_path)
+    text = ''
+    for page_num, page in enumerate(reader.pages):
+        text += page.extract_text()
+    return text
+
+def text_to_audio(text, audio_path):
+    tts = gTTS(text)
+    tts.save(audio_path)
+
+def main():
+    pdf_path = input("Enter the path to the PDF file: ").strip('"') # Remove double quotes around the file path
+
+    # Start timing
+    start_time = time.time()
+
+    # Convert PDF to text
+    text = pdf_to_text(pdf_path)
+
+    # Generate text file path based on PDF file name
+    text_filename = os.path.splitext(os.path.basename(pdf_path))[0] + '.txt'
+    text_path = os.path.join(os.path.dirname(pdf_path), text_filename)
+
+    # Save text to a file
+    with open(text_path, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+    # Generate audio file path based on PDF file name
+    audio_filename = os.path.splitext(os.path.basename(pdf_path))[0] + '.mp3'
+    audio_path = os.path.join(os.path.dirname(pdf_path), audio_filename)
+
+    # Convert text to audio
+    text_to_audio(text, audio_path)
+
+    # End timing
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Script execution time: {execution_time:.2f} seconds")
+
+    print('Conversion complete.')
+
+if __name__ == '__main__':
+    main()
